bc6cbfa2b1e13f963331798c358f43b5996233fd
Yunus Ünver <yunus.unver@bilgeadam.com>
2020-08-30 01:27:34 +0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+
+import Login from "./view/Login/Login.vue";
+import Home from "./view/Home/Home.vue";
+import CodeList from "./view/Code/CodeList.vue";
+
+const router = new VueRouter({
+  routes: [
+    {
+      path: "/",
+      component: Home,
+      children: [{ path: "codelist", component: CodeList }],
+    },
+    { path: "/login", component: Login },
+  ],
+  mode: "history",
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+  router,
+}).$mount("#app");
