51a9aea624c12269081e8b6fd37a93877325706b
Yunus Ünver <yunus.unver@bilgeadam.com>
2020-09-03 00:52:07 +0300

    Add logout button.

--- src/components/Layout/AppBar/AppBar.vue
+++ src/components/Layout/AppBar/AppBar.vue
@@ -1,5 +1,10 @@
 <template>
-  <v-app-bar :clipped-left="$vuetify.breakpoint.lgAndUp" app color="blue darken-3" dark>
+  <v-app-bar
+    :clipped-left="$vuetify.breakpoint.lgAndUp"
+    app
+    color="blue darken-3"
+    dark
+  >
     <v-app-bar-nav-icon @click.stop="toggleDrawer()"></v-app-bar-nav-icon>
     <v-toolbar-title style="width: 300px" class="ml-0 pl-4">
       <span class="hidden-sm-and-down">omission</span>
@@ -19,20 +24,17 @@
     <v-btn icon>
       <v-icon>mdi-bell</v-icon>
     </v-btn>
-    <v-btn icon large>
-      <v-avatar size="32px" item>
-        <v-img src="https://cdn.vuetifyjs.com/images/logos/logo.svg" alt="Vuetify"></v-img>
-      </v-avatar>
+    <v-btn icon to="/login">
+      <v-icon>mdi-logout</v-icon>
     </v-btn>
   </v-app-bar>
 </template>
 
 <script>
-import {methods} from "./sections/methods";
+import { methods } from "./sections/methods";
 export default {
-    methods:methods
+  methods: methods,
 };
 </script>
 
-<style>
-</style>
+<style></style>
